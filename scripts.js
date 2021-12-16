4185e1899da587a560c6a8ef4e2b754c91f76700
LetiDiPaula <96254586+LetiDiPaula@users.noreply.github.com>
2021-12-16 14:06:25 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var nome = " Leticia";
+var notaDoPrimeiroBimestre = 9.344;
+var notaFinal = notaDoPrimeiroBimestre + 1;
+var notaDoSegundoBimestre = 7.546;
+var notaDoTerceiroBimestre = 4.235;
+var notaDoQuartoBimestre = 2.357;
+
+var notaFinal =
+  (notaDoPrimeiroBimestre +
+    notaDoSegundoBimestre +
+    notaDoTerceiroBimestre +
+    notaDoQuartoBimestre) /
+  4;
+
+var notaFixada = notaFinal.toFixed(0);
+
+console.log("Bem vindo" + nome);
+console.log(notaFixada);
+
+// isso é um comentário
+
+// revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
